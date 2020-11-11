f96be641882de9453862d6e07472d90864a0f57a
Danny Tiesling <danny@zorroa.com>
2020-11-11 10:17:45 -0800

    Adds support for configuring a postgres database using environment variables passed to the container.

--- django_backend/scrounger/settings.py
+++ django_backend/scrounger/settings.py
@@ -82,13 +82,26 @@ WSGI_APPLICATION = 'scrounger.wsgi.application'
 # the appropriate configuration
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASE_PATH = os.environ.get('DATABASE_PATH', os.path.join(BASE_DIR, 'db.sqlite3'))
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATABASE_PATH,
+
+if os.environ.get('DB_BACKEND') == 'postgres':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('PG_DB_NAME', 'scrounger'),
+            'USER': os.environ.get('PG_DB_USER', 'scrounger'),
+            'PASSWORD': os.environ['PG_DB_PASSWORD'],
+            'HOST': os.environ.get('PG_DB_HOST', '127.0.0.1'),
+            'PORT': os.environ.get('PG_DB_PORT', '5432')
+        }
     }
-}
+else:
+    DATABASE_PATH = os.environ.get('DATABASE_PATH', os.path.join(BASE_DIR, 'db.sqlite3'))
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': DATABASE_PATH,
+        }
+    }
 
 
 # Password validation
