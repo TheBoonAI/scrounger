81336f7c056329e919b3aa2d1e93160b3e0be939
octaviaaris <oapayne@gmail.com>
2020-11-12 12:51:46 -0800

    add tests

--- react_frontend/README.md
+++ react_frontend/README.md
@@ -8,3 +8,8 @@ Install dependencies: `npm install` (or `npm ci`)
 
 Run `npm run dev` to start a local development node server on
 http://localhost:3000
+
+### Testing
+
+Scrounger uses [Jest](https://jestjs.io/docs/en/22.x/getting-started) and
+[React Testing Library](https://testing-library.com/docs/react-testing-library/intro).
---
+++ react_frontend/src/Login/__tests__/index.spec.js
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+
+import Login from '..'
+
+jest.mock('../../Authentication/helpers')
+
+describe('<Login />', () => {
+  test('allows the user to log in', () => {
+    const mockLogin = jest.fn()
+
+    require('../../Authentication/helpers').__setMockLogin(mockLogin)
+
+    render(<Login />)
+
+    fireEvent.change(screen.getByLabelText('Username'), {
+      target: { value: 'mockUsername' },
+    })
+
+    fireEvent.change(screen.getByLabelText('Password'), {
+      target: { value: 'mockPassword' },
+    })
+
+    expect(screen.getAllByDisplayValue('mockUsername')).toHaveLength(1)
+    expect(screen.getAllByDisplayValue('mockPassword')).toHaveLength(1)
+
+    fireEvent.click(screen.getByText('Sign In'))
+
+    expect(mockLogin.mock.calls[0][0].username).toEqual('mockUsername')
+    expect(mockLogin.mock.calls[0][0].password).toEqual('mockPassword')
+  })
+})
