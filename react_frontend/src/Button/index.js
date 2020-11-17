acda79d2d84a610afcc82ee00d40f1c73e99864b
timothychin <tim.chin@zorroa.com>
2020-11-17 15:17:53 -0800

    Remove Button variants

--- react_frontend/src/Button/index.js
+++ react_frontend/src/Button/index.js
@@ -1,14 +1,9 @@
 import PropTypes from 'prop-types'
 
-const VARIANTS = {
-  primary:
-    'h-auto py-2 px-4 rounded bg-green-500 hover:bg-green-700 whitespace-no-wrap',
-}
-
-const Button = ({ variant, onClick, children, ...props }) => {
+const Button = ({ onClick, children, ...props }) => {
   return (
     <button
-      className={VARIANTS[variant]}
+      className="h-auto py-2 px-4 rounded bg-green-500 hover:bg-green-700 whitespace-no-wrap"
       onClick={onClick}
       type="button"
       {...props}
@@ -19,7 +14,6 @@ const Button = ({ variant, onClick, children, ...props }) => {
 }
 
 Button.propTypes = {
-  variant: PropTypes.oneOf(Object.keys(VARIANTS)).isRequired,
   onClick: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
 }
