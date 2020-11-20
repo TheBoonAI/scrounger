233a09af177731f9236a690e1d9c9ff276b623ab
Danny Tiesling <37917843+danny-zorroa@users.noreply.github.com>
2020-11-20 10:31:23 -0800

    Changes the django settings file to use an environment variable for the secret key or fall back to a randomly generated one. This prevents anyone forking the repo from forgetting to change the secret key. (#23)

--- django_backend/scrounger/settings.py
+++ django_backend/scrounger/settings.py
@@ -13,6 +13,7 @@ import json
 import os
 import re
 
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nnb4iz@t!(+^#vg5dl4@n7z^ux2ub8yxq0wjj1!dcbmr-4tjhy'
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DEBUG') == 'true':
