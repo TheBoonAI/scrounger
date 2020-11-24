e8474dfcb5e6f230b928a8216871c99df71f84c7
Danny Tiesling <danny@zorroa.com>
2020-11-24 08:16:20 -0800

    Fixes a bug in the Django settings file that caused the secret key to cycle on every request.

--- django_backend/scrounger/settings.py
+++ django_backend/scrounger/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.environ.get('SECRET_KEY', 'nnb4iz@t!(+^#vg5dl4@n7z^ux2ub8yxq0wjj1!dcbmr-4tjhy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DEBUG') == 'true':
