5f1eaca46ecf877beaf0c724faf32168da687106
Damien Varron <damien@varron.fr>
2021-06-08 09:12:34 -0700

    update tailwind@2

--- react_frontend/tailwind.config.js
+++ react_frontend/tailwind.config.js
@@ -1,9 +1,5 @@
 // https://tailwindcss.com/docs/configuration
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: ['./pages/**/*.js', './src/**/*.js'],
   variants: {
     opacity: ['responsive', 'hover', 'focus', 'group-hover', 'group-focus'],
